aef09c3a6dc1dd57291f924f2894ba871f9a682d
archanabethala-code <archanabethala@gmail.com>
2026-02-06 14:19:14 +0530

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,7 +1,8 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+
+console.log('Zenith App Initializing...');
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
